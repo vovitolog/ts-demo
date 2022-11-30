81595c591a586d7b80d202e2907af5e0d03db938
vovitolog <vovitolog@gmail.com>
2022-11-30 20:27:48 +0300

    refactor: add callback into Star component

--- src/components/UncontrolledRating/UncontrolledRating.tsx
+++ src/components/UncontrolledRating/UncontrolledRating.tsx
@@ -1,37 +1,28 @@
 import React, { useState } from "react";
-//
-// type RatingProps = {
-//   value: 0 | 1 | 2 | 3 | 4 | 5;
-// };
 
 export function UncontrolledRating() {
   const [value, setValue] = useState(0);
   return (
     <div>
-      <Star selected={value > 0} />
-      <button onClick={() => setValue(1)}>1</button>
-      <Star selected={value > 1} />
-      <button onClick={() => setValue(2)}>2</button>
-      <Star selected={value > 2} />
-      <button onClick={() => setValue(3)}>3</button>
-      <Star selected={value > 3} />
-      <button onClick={() => setValue(4)}>4</button>
-      <Star selected={value > 4} />
-      <button onClick={() => setValue(5)}>5</button>
+      <Star selected={value > 0} setValue={setValue} value={1} />
+      <Star selected={value > 1} setValue={setValue} value={2} />
+      <Star selected={value > 2} setValue={setValue} value={3} />
+      <Star selected={value > 3} setValue={setValue} value={4} />
+      <Star selected={value > 4} setValue={setValue} value={5} />
     </div>
   );
 }
 
 type StarPropsType = {
   selected: boolean;
+  value: 1 | 2 | 3 | 4 | 5;
+  setValue: (value: 1 | 2 | 3 | 4 | 5) => void;
 };
 
 function Star(props: StarPropsType) {
-  return props.selected ? (
-    <span>
-      <b>Star</b>
+  return (
+    <span onClick={() => props.setValue(props.value)}>
+      {props.selected ? <b>Star</b> : "Star"}
     </span>
-  ) : (
-    <span>Star</span>
   );
 }
