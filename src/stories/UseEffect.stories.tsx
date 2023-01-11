57adab2ed8d7447ab46e0ab9823a97ebb3b98c6f
vovitolog <vovitolog@gmail.com>
2023-01-11 21:24:38 +0300

    feat: add setInterval useEffect example

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -15,13 +15,13 @@ export const SimpleExample = () => {
     document.title = counter.toString();
   });
 
-  useEffect(()=>{
-      console.log("Effect on component Mount");
-  },[])
+  useEffect(() => {
+    console.log("Effect on component Mount");
+  }, []);
 
-    useEffect(()=>{
-      console.log("Effect on counter change");
-  },[counter])
+  useEffect(() => {
+    console.log("Effect on counter change");
+  }, [counter]);
 
   return (
     <>
@@ -29,8 +29,9 @@ export const SimpleExample = () => {
         onClick={() => {
           setCounter(counter + 1);
         }}
-      >Counter</button>
-
+      >
+        Counter
+      </button>
       <button
         onClick={() => {
           setFake(fake + 1);
@@ -39,6 +40,47 @@ export const SimpleExample = () => {
         Fake
       </button>
       Hello, {counter} {fake}
+    </>
+  );
+};
+
+export const SetTimeoutExample = () => {
+  const [counter, setCounter] = useState(1);
+  const [fake, setFake] = useState(1);
+  const [hours, setHours] = useState(new Date().getHours());
+  const [minutes, setMinutes] = useState(new Date().getMinutes());
+  const [seconds, setSeconds] = useState(new Date().getSeconds());
+
+  console.log("SetTimeoutExample");
+
+  useEffect(() => {
+    setInterval(() => {
+      //console.log("tick");
+      setCounter((state) => state + 1);
+      setSeconds((state) => new Date().getSeconds());
+      setMinutes((state) => new Date().getMinutes());
+      setHours((state) => new Date().getHours());
+     // console.log(new Date().getHours());
+    }, 1000);
+  }, []);
+
+  return (
+    <>
+      {/*<button*/}
+      {/*  onClick={() => {*/}
+      {/*    setCounter(counter + 1);*/}
+      {/*  }}*/}
+      {/*>*/}
+      {/*  Counter*/}
+      {/*</button>*/}
+      {/*<button*/}
+      {/*  onClick={() => {*/}
+      {/*    setFake(fake + 1);*/}
+      {/*  }}*/}
+      {/*>*/}
+      {/*  Fake*/}
+      {/*</button>*/}
+      Hello, counter: {counter} fake: {fake} TIME: {hours}:{minutes}:{seconds}
     </>
   );
 };
